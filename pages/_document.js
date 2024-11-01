00c4dba40a271592eb433a2e6fba5d4dc2179c91
JDs2019sk <joeldias1907@gmail.com>
2024-11-01 23:22:26 +0000

    update v1

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className="antialiased">
         <Main />
         <NextScript />
